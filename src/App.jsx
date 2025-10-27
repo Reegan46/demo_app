1fcaa10b79189ea5181ea4019a3b1a152bd4af01
Reegan <reeganr897@gmail.com>
2025-10-28 03:32:35 +0530

    initial commit to dev branch

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <h2>Thius is branch3</h2>
+      <h4>This is branch develop</h4>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
